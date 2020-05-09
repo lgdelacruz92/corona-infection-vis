0ad42dd64306b63dbb8913678237ed88f263ffa5
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-05-09 08:14:10 -0700

    Removed Physics

--- src/person.p5.js
+++ src/person.p5.js
@@ -1,46 +1,16 @@
-import { canvasWindow } from './canvaswindow.p5';
-
+import matter from 'matter-js';
 
 class Person {
-    constructor(x, y, s, v) {
-        this.pos = s.createVector(x, y);
-        this.vel = v || s.createVector(s.random(5), s.random(5));
+    constructor(x, y, s, e) {
+        this.body = matter.Bodies.circle(x, y, 10);
+        matter.World.add(e.world, this.body);
         this.s = s;
-        this.m = 1;
-    }
-
-    setVel(vel) {
-        this.vel = vel;
-    }
-
-    isCollide(pOther) {
-        const _x = pOther.pos.x - this.pos.x;
-        const _y = pOther.pos.y - this.pos.y;
-        const d = this.s.sqrt(this.s.pow(_x, 2) + this.s.pow(_y, 2));
-        return d < 20;
-    }
-
-    update() {
-        this.pos.add(this.vel);
     }
 
     draw() {
-        if (this.pos.y + 10 > canvasWindow.h) {
-            this.vel.y *= -1;    
-        }
-        if (this.pos.x + 10 > canvasWindow.w) {
-            this.vel.x *= -1;
-        }
-        if (this.pos.y - 10 < 0) {
-            this.vel.y *= -1;
-        }
-
-        if (this.pos.x - 10 < 0) {
-            this.vel.x *= -1;
-        }
-
+        const pos = this.body.position;
         this.s.fill(255);
-        this.s.circle(this.pos.x, this.pos.y, 20);
+        this.s.circle(pos.x, pos.y, 20);
     }
 }
 export default Person;
--- src/simulation.p5.js
+++ src/simulation.p5.js
@@ -1,34 +1,28 @@
 import p5 from 'p5';
 import Person from './person.p5';
 import { canvasWindow } from './canvaswindow.p5';
-import Physics from './physics.p5';
+import matter from 'matter-js';
 
 const sketch = (s) => {
     let p;
     let p1;
-    let physics;
+    let engine;
     s.setup = () => {
+        engine = matter.Engine.create();
+        engine.world.gravity.scale = 0.000001;
         s.createCanvas(canvasWindow.w, canvasWindow.h);
         s.background(0);
         s.fill(255);
-        physics = new Physics(s);
-        p = new Person(10, 10, s);
-        p1 = new Person(300, 10, s);
+
+        p = new Person(10, 10, s, engine);
+        p1 = new Person(300, 10, s, engine);
     }
 
     s.draw = () => {
+        matter.Engine.run(engine);
         s.background(0);
         p.draw();
         p1.draw();
-
-        if (p.isCollide(p1)) {
-            const vels = physics.collide(1, 1, p.vel, p1.vel);
-            p.setVel(vels.v1_prime);
-            p1.setVel(vels.v2_prime);
-        }
-
-        p.update(p1);
-        p1.update(p);
     }
 }
 let s = new p5(sketch);
